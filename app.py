422fdc1b0e88c37c3188797607c948130268a5aa
NDKhang2212 <nguyenduckhang0445@gmail.com>
2024-06-17 14:38:57 +0700

    Modify app.py

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ from functools import wraps
 from flask_session import Session
 import sqlite3
 import requests
-import hashlib 
+import hashlib
 
 
 app = Flask(__name__)
@@ -15,6 +15,8 @@ connection = sqlite3.connect("track_me_run.db")
 cursor = connection.cursor()
 
 account = {}
+
+
 def login_required(f):
     """
     Decorate routes to require login.
@@ -29,6 +31,7 @@ def login_required(f):
         return f(*args, **kwargs)
 
     return decorated_function
+
 
 def apology(message, code=400):
     """Render message as an apology to user."""
@@ -57,10 +60,12 @@ def apology(message, code=400):
 # def hash_password(password):
 #     return hashlib.sha256(password.encode()).hexdigest()
 
+
 @app.route("/")
 @login_required
 def home():
     return render_template("home.html")
+
 
 @app.route("/login", methods=["POST", "GET"])
 def login():
@@ -81,7 +86,7 @@ def login():
             n = row[2]
             p = row[3]
             i = row[0]
-        if username != n or  password != p:
+        if n != username or password != p:
             return apology("Incorrect username or password")
         session["user_id"] = i
         connection.close()
@@ -89,6 +94,7 @@ def login():
     else:
         connection.close()
         return render_template("login.html")
+
 
 @app.route("/signup", methods=["GET", "POST"])
 def signup():
@@ -102,12 +108,14 @@ def signup():
         rows = cursor.execute("SELECT username FROM users")
         for row in rows:
             if username == row[0]:
-                return apology("Username already exists")        
-        
+                return apology("Username already exists")
+
         # Hash the password before storing
-        cursor.execute("INSERT INTO users (username, password) VALUES (?,?)", (username,password,))
+        cursor.execute(
+            "INSERT INTO users (username, password) VALUES (?,?)", (username, password,))
         connection.commit()
-        rows = cursor.execute("SELECT * FROM users WHERE username = ?", (username,))
+        rows = cursor.execute(
+            "SELECT * FROM users WHERE username = ?", (username,))
         session["user_id"] = [row[0] for row in rows][0]
         connection.close()
         return redirect("/")
@@ -115,7 +123,8 @@ def signup():
         connection.close()
         return render_template("signup.html")
 
-@app.route("/changepass",methods=['GET','POST'])
+
+@app.route("/changepass", methods=['GET', 'POST'])
 @login_required
 def changepass():
     connection = sqlite3.connect("track_me_run.db")
@@ -124,17 +133,21 @@ def changepass():
         check = False
         confirm_check = False
         old_password = request.form.get("old_password")
-        rows = cursor.execute("SELECT password FROM users WHERE id = ?",(session['user_id'],))
+        rows = cursor.execute(
+            "SELECT password FROM users WHERE id = ?", (session['user_id'],))
         for row in rows:
             if old_password != row[0]:
                 check = True
-                return render_template("changepass.html", check = check) #render lai html voi dong "Your submission is incorrect"
+                # render lai html voi dong "Your submission is incorrect"
+                return render_template("changepass.html", check=check)
         new_password = request.form.get("new_password")
         confirm_password = request.form.get("confirm_password")
         if new_password != confirm_password:
             confirm_check = True
-            return render_template("changepass.html", confirm_check = confirm_check) #render lai html voi dong "Wrong password resubmission"
-        cursor.execute("UPDATE users SET password = ? WHERE id = ?",(new_password,session['user_id'],))
+            # render lai html voi dong "Wrong password resubmission"
+            return render_template("changepass.html", confirm_check=confirm_check)
+        cursor.execute("UPDATE users SET password = ? WHERE id = ?",
+                       (new_password, session['user_id'],))
         connection.commit()
         connection.close()
         return redirect("/")
@@ -142,12 +155,16 @@ def changepass():
         connection.close()
         return render_template("changepass.html")
 # TO BE CONTINUED
-@app.route("/checkprofile",methods=['GET','POST'])
+
+
+@app.route("/checkprofile", methods=['GET', 'POST'])
 @login_required
 def checkprofile():
     connection = sqlite3.connect("track_me_run.db")
     cursor = connection.cursor()
     return render_template("checkprofile.html")
+
+
 @app.route("/logout")
 def logout():
     """Log user out"""
@@ -155,8 +172,3 @@ def logout():
     session.clear()
     # Redirect user to login form
     return redirect(url_for("login"))
-
-
-
-
-    
