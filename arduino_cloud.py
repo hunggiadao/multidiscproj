791079a6876aa5d3de0b80a570d39e4b4b4725b3
Hung <hunggiadao123@gmail.com>
2024-06-26 10:53:28 +0700

    added .sh to run everything with 1 click

--- arduino_cloud.py
+++ arduino_cloud.py
@@ -7,6 +7,18 @@ import iot_api_client as iot
 from iot_api_client.rest import ApiException
 from iot_api_client.configuration import Configuration
 
+def formatted_time_return(puttime):
+	# if (type(puttime) != type(datetime.datetime)):
+	# 	raise TypeError(f"{puttime} attribute must be set to an instance of datetime.datetime")
+	formatted_month = ('00' + str(buffer_time.month))[-2:]
+	formatted_date = ('00' + str(buffer_time.day))[-2:]
+	formatted_hour = ('00' + str(buffer_time.hour - 7))[-2:]
+	formatted_minute = ('00' + str(buffer_time.minute))[-2:]
+	formatted_second = ('00' + str(buffer_time.second))[-2:]
+	formatted_time = str(buffer_time.year) + '-' + formatted_month + '-' + formatted_date + 'T' + formatted_hour + ':' + formatted_minute + ':' + formatted_second + 'Z'
+	return formatted_time
+	print(f"Formatted:\t{formatted_time}")
+
 # Get your token, don't change client ID and client secret
 oauth_client = BackendApplicationClient(client_id="967VT2YXvzhTjgPr7YqJnwpx4gv9LPj3")
 token_url = "https://api2.arduino.cc/iot/v1/clients/token"
@@ -32,15 +44,11 @@ with iot.ApiClient(client_config) as client:
 	api = iot.PropertiesV2Api(client)
 	thing_id = "95f40050-a59d-4624-80b2-01d27ebbb824" # iPhone thing
 	# thing_id = "a798decb-21e5-4b5f-a659-f4941437ae8f" # test_thing
-# client = iot.ApiClient(client_config)
-
-# Change this based on the device being used
-# thing_id = "95f40050-a59d-4624-80b2-01d27ebbb824"
 
 # Custom variables for debugging
 looping = True # whether the program continuously fetches data in a loop
-minutes_ago = 1 # how far back in time should the program fetch data, more minutes means longer fetch time
-sleep_duration = float(0.5) # how many seconds should the program wait before fetching again
+minutes_ago = 0 # how far back in time should the program fetch data, more minutes means longer fetch time
+sleep_duration = float(1) # how many seconds should the program wait before fetching again
 
 # init data for Properties API interactions
 # api = iot.PropertiesV2Api(client)
@@ -57,18 +65,26 @@ while (True):
 	try:
 		now_time = datetime.now()
 		print(f"Now:\t{now_time}")
-		prev_time = now_time + timedelta(minutes=-minutes_ago)
-		print(f"{minutes_ago} mins ago:\t{prev_time}")
-		formatted_month = ('00' + str(prev_time.month))[-2:]
-		formatted_date = ('00' + str(prev_time.day))[-2:]
-		formatted_hour = ('00' + str(prev_time.hour - 7))[-2:]
-		formatted_minute = ('00' + str(prev_time.minute))[-2:]
-		formatted_second = ('00' + str(prev_time.second))[-2:]
-		formatted_time = str(prev_time.year) + '-' + formatted_month + '-' + formatted_date + 'T' + formatted_hour + ':' + formatted_minute + ':' + formatted_second + 'Z'
+		buffer_time = now_time + timedelta(minutes=-minutes_ago)
+		# print(f"{minutes_ago} mins ago:\t{buffer_time}")
+		formatted_time = formatted_time_return(buffer_time)
 		print(f"Formatted:\t{formatted_time}")
 
 		# resp = api.properties_v2_list(thing_id)
 		resp = api.properties_v2_show(thing_id, gps_id)
+		# print(type(resp._value_updated_at))
+		if (resp._value_updated_at != prev_time_record):
+			# new record found
+			prev_time_record = resp._value_updated_at
+			last_val = resp._last_value
+			# print(f"{last_val}")
+			# print(type(last_val))
+			print(f"{last_val['lat']} {last_val['lon']}")
+			# print(f"{type(last_val['lat'])} {type(last_val['lon'])}")
+		else:
+			print("No new records since last fetch")
+
+		# this code is used for timeseries
 		# resp = api.properties_v2_timeseries(thing_id, property_id, desc=True, _from=formatted_time, interval=1)
 		# if (len(resp.data) > 0):
 		# 	print(f"Batch size: {len(resp.data)}")
@@ -79,9 +95,10 @@ while (True):
 		# 		print("No new records since last fetch")
 		# else:
 		# 	print(f"No records in last {minutes_ago} minutes")
+			
 		# print(resp)
 		# print(f"{resp.value_updated_at}\tGyroscope_X: {resp.last_value}")
-		print(f"{resp._last_value.id}")
+		
 	except ApiException as e:
 		print("Got an exception: {}".format(e))
 
